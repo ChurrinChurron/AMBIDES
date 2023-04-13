b7c75213db423c47b5866c0cc2ba6cb7c552c924
ChurrinChurron <marianafantasy8@gmail.com>
2023-04-12 21:22:09 -0400

    AMBIDES

---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+let abrirMenu = document.querySelector("#btnMovil");
+let cerrarMenu = document.querySelector("#btnMovil_cerrar");
+let menuMovil = document.querySelector(".menu__movil");
+let abrirListaMov = document.querySelector("#linkServiciosMovil");
+let listaMovil = document.querySelector(".links__servicios__movil");
+let abrirModal = document.querySelector("#modalLogo");
+let modal = document.querySelector(".modal");
+let cerrarModal = document.querySelector("#cerrarModal");
+
+/*FUNCION MENU MOVIL*/
+
+abrirMenu.addEventListener("click", () => {
+
+    menuMovil.classList.remove("ocultar");
+});
+
+cerrarMenu.addEventListener("click", () => {
+
+    menuMovil.classList.add("ocultar");
+});
+
+/*FUNCION LISTA MOVIL*/
+
+abrirListaMov.addEventListener("click", () => {
+
+    listaMovil.classList.toggle("abrir");
+});
+
+/*FUNCION MODAL*/
+
+abrirModal.addEventListener("click", () => {
+
+    modal.classList.remove("cerrar");
+});
+
+cerrarModal.addEventListener("click", () => {
+
+    modal.classList.add("cerrar");
+});
